2d8d1fafd109ca56660c4d33be706a7eacbaebec
ziggytron <ziggytron565@gmail.com>
2025-01-15 17:04:26 -0500

    pose estimation???

--- src/main/java/frc/robot/subsystems/PoseEstimation.java
+++ src/main/java/frc/robot/subsystems/PoseEstimation.java
@@ -10,19 +10,25 @@ import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator3d;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kSwerve;
 import frc.robot.Constants.kVision;
 
 public class PoseEstimation extends SubsystemBase {
-  private final SwerveSubsystem m_SwerveSubsystem;
-  private final Vision m_Vision;
+  private SwerveSubsystem m_SwerveSubsystem;
+  private Vision m_Vision;
+
+  /** Creates a new PoseEstimation. */
+
+  public PoseEstimation(SwerveSubsystem swerveSubsystem, Vision vision) {
+    m_SwerveSubsystem = swerveSubsystem;
+    m_Vision = vision;
+  };
 
   PhotonPoseEstimator PoseEstimator1 = new PhotonPoseEstimator(
     kVision.aprilTagFieldLayout, 
@@ -41,12 +47,7 @@ public class PoseEstimation extends SubsystemBase {
     PoseStrategy.CLOSEST_TO_REFERENCE_POSE, 
     kVision.camera4Position);
   
-  /** Creates a new PoseEstimation. */
-
-  public PoseEstimation(SwerveSubsystem swerveSubsystem, Vision vision) {
-    m_SwerveSubsystem = swerveSubsystem;
-    m_Vision = vision;
-
+  private Pose3d getPose3d() {
     Pose3d pose3d = new Pose3d();
     var result = m_Vision.camera1.getLatestResult();
     boolean hasTargets = result.hasTargets();
@@ -56,17 +57,25 @@ public class PoseEstimation extends SubsystemBase {
         pose3d = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), kVision.aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(), kVision.camera1Position);
       }
     }
-    SwerveDrivePoseEstimator3d swervePoseEstimator1 = new SwerveDrivePoseEstimator3d(
-      kSwerve.kinematics,
-      m_SwerveSubsystem.navX.getRotation3d(),
-      m_SwerveSubsystem.modulePositions, 
-      pose3d);
-    
+    return pose3d;
+  }
 
-  };
+  SwerveDrivePoseEstimator3d swervePoseEstimator = new SwerveDrivePoseEstimator3d(
+    kSwerve.kinematics,
+    m_SwerveSubsystem.navX.getRotation3d(),
+    m_SwerveSubsystem.modulePositions, 
+    getPose3d());
 
+  Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose3d prevEstimatedRobotPose, PhotonPipelineResult cameraResult) {
+    PoseEstimator1.setReferencePose(prevEstimatedRobotPose);
+    return PoseEstimator1.update(cameraResult);
+  }
   @Override
   public void periodic() {
+    PhotonPipelineResult camera1res = m_Vision.camera1.getLatestResult();
+    Optional<EstimatedRobotPose> camera1pose = getEstimatedGlobalPose(swervePoseEstimator.getEstimatedPosition(), camera1res);
+    swervePoseEstimator.addVisionMeasurement(camera1pose.get().estimatedPose,camera1pose.get().timestampSeconds);
+
     // PoseEstimator1.setReferencePose();
     // return PoseEstimator1.update();
   }
