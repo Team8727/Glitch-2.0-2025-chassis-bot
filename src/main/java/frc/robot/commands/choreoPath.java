400bab917e2dcfa50e740c6bc05aee83e9e55c53
ziggytron <ziggytron@gmail.com>
2025-02-01 15:04:40 -0500

    auto align works mostly

--- src/main/java/frc/robot/commands/choreoPath.java
+++ src/main/java/frc/robot/commands/choreoPath.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
-import choreo.trajectory.SwerveSample;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.PoseEstimatior;
