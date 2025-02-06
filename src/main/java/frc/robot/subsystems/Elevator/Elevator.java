24c6d5658bd61c9298badc176e9821d33eb9aff6
GriffinGryffinGryphon <griffinlowings@gmail.com>
2025-02-06 16:33:13 -0500

    Elevator Subsystem

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -4,11 +4,66 @@
 
 package frc.robot.subsystems.Elevator;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import static frc.robot.utilities.SparkConfigurator.getFollowerMax;
+import static frc.robot.utilities.SparkConfigurator.getSparkMax;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.kElevator;
+import frc.robot.utilities.SparkConfigurator.LogData;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Elevator extends SubsystemBase {
+  private final SparkMax elevatorMotorR;
+  private final SparkMax elevatorMotorL;
+  private final SparkMaxConfig motorRConfig;
+  private final SparkMaxConfig motorLConfig;
   /** Creates a new Elevator. */
-  public Elevator() {}
+  public Elevator() {
+    elevatorMotorR = getSparkMax(
+      kElevator.elevatorMotorRCANID, 
+      SparkMax.MotorType.kBrushless,
+      true,
+      Set.of(),
+      Set.of(
+        LogData.POSITION,
+        LogData.VELOCITY,
+        LogData.VOLTAGE,
+        LogData.CURRENT));
+    elevatorMotorL = getFollowerMax(
+      elevatorMotorR, 
+      kElevator.elevatorMotorLCANID, 
+      SparkMax.MotorType.kBrushless, 
+      true);
+
+    motorRConfig = new SparkMaxConfig();
+    motorRConfig
+      .smartCurrentLimit(40)
+      .idleMode(IdleMode.kBrake)
+      .closedLoop
+      .velocityFF(0)
+      .pid(0, 0, 0)
+      .maxMotion
+      .maxAcceleration(0)
+      .maxAcceleration(0)
+      .allowedClosedLoopError(0);
+    motorLConfig = new SparkMaxConfig();
+    motorLConfig
+      .smartCurrentLimit(40)
+      .idleMode(IdleMode.kBrake)
+      .closedLoop
+      .velocityFF(0)
+      .pid(0, 0, 0)
+      .maxMotion
+      .maxAcceleration(0)
+      .maxAcceleration(0)
+      .allowedClosedLoopError(0);
+  }
 
   @Override
   public void periodic() {
