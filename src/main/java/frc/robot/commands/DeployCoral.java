b699d2a1862b800c8d991f8c216402150d5e0476
ziggytron <ziggytron565@gmail.com>
2025-02-05 19:04:01 -0500

    test

--- src/main/java/frc/robot/commands/DeployCoral.java
+++ src/main/java/frc/robot/commands/DeployCoral.java
@@ -34,7 +34,7 @@ public class DeployCoral extends Command {
   public void initialize() {
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  // Called every time the scheduler runs while the command is scheduled
   @Override
   public void execute() {
     flywheelSpeed = 0;
