0c952d82a6f5937a534a991bf076aba630bab683
ziggytron <ziggytron565@gmail.com>
2025-02-10 17:37:38 -0500

    changed var names

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -75,7 +75,7 @@ public class LEDSubsystem extends SubsystemBase { // Fixed class name
 
   // Elevator progress bar pattern
   private LEDPattern elevatorProgressMap = LEDPattern.progressMaskLayer(
-    () -> m_elevator.targetHeight / kElevator.ElevatorPosition.L4.getRotations());
+    () -> m_elevator.targetHeight / kElevator.ElevatorPosition.L4.getOutputRotations());
   private LEDPattern elevatorProgressBase = LEDPattern.gradient(GradientType.kDiscontinuous, m_green, m_yellow, m_orange);
   public LEDPattern elevatorProgress = elevatorProgressBase.mask(elevatorProgressMap);
 
