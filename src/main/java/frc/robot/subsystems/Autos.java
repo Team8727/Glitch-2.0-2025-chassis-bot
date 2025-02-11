8576f54b1c253cf83fe41efa57df49d101bcd71b
ziggytron <ziggytron565@gmail.com>
2025-02-11 16:09:43 -0500

    paths

--- src/main/java/frc/robot/subsystems/Autos.java
+++ src/main/java/frc/robot/subsystems/Autos.java
@@ -15,16 +15,22 @@ import frc.robot.Constants.kSwerve;
 
 public class Autos extends SubsystemBase {
   /** Creates a new Autos. */
-  public Autos() {}
+  public Autos() {
+
+    // PathPlannerPath exampleChoreoTraj = PathPlannerPath.fromChoreoTrajectory("Example Choreo Traj");
 
     /* Add paths to the hashmap using this format:
-     paths.put("<Name>", PathPlannerPath.fromPathFile("<path file name>"));
-     ex:
-     paths.put("Crazy auto", swerve.followPathWithEventsCommand(paths.get("crazy_auto")));
-  */
-  private void loadPaths() {
+      paths.put("<Name>", PathPlannerPath.fromPathFile("<path file name>"));
+      ex:
+      paths.put("Crazy auto", swerve.followPathWithEventsCommand(paths.get("crazy_auto")));
+    */
+    
     //paths.put("EXAMPLE", PathPlannerPath.fromChoreoTrajectory("EXAMPLE"));
+
   }
+
+
+
 
   public Command align(Pose2d goal) {
     return AutoBuilder.pathfindToPose(
