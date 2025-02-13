a60fc33afac0c241f59bdb06ac6d56553db4811e
Charlie Huguenard <charliehuge@users.noreply.github.com>
2025-02-13 13:46:13 -0500

    added example unit test and excluded tests from regular deploy/build

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestExample {
+    @Test
+    void testExample() {
+        assertEquals(1, 1);
+    }
+}
