22621a8f1daa1e874bc06561b4be9be0e6e2a86b
ziggytron <ziggytron565@gmail.com>
2025-02-13 16:19:21 -0500

    drive cmd to sub

--- src/main/java/frc/robot/commands/DriveCommands/DriveCmd.java
+++ src/main/java/frc/robot/commands/DriveCommands/DriveCmd.java
@@ -3,53 +3,46 @@ package frc.robot.commands.DriveCommands;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kSwerve;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.utilities.NetworkTableLogger;
+
 import java.util.function.Supplier;
 
-public class DriveCmd extends Command {
+public class DriveCmd extends SubsystemBase {
 
   private final SwerveSubsystem m_SwerveSubsystem;
-  private final Supplier<ChassisSpeeds> m_speeds;
-  private final Supplier<Boolean> m_fieldOrientedFunction;
+  private final Boolean m_fieldOrientedFunction;
+  private SwerveModuleState[] moduleStates = kSwerve.kinematics.toSwerveModuleStates(new ChassisSpeeds());
+  private final NetworkTableLogger logger = new NetworkTableLogger("modules");
+
 
   public DriveCmd(
       SwerveSubsystem swerveSubsystem,
-      Supplier<ChassisSpeeds> speeds,
-      Supplier<Boolean> fieldOrientedFunction) {
+      Boolean fieldOrientedFunction) {
     m_SwerveSubsystem = swerveSubsystem;
-    m_speeds = speeds;
     m_fieldOrientedFunction = fieldOrientedFunction;
-    addRequirements(swerveSubsystem);
   }
 
-  @Override
-  public void initialize() {}
-
-  @Override
-  public void execute() {
+  public void setChassisSpeeds(ChassisSpeeds speeds) {
 
     // Set chassis speed
     ChassisSpeeds finalChassisSpeeds;
-    if (m_fieldOrientedFunction.get()) {
+    if (m_fieldOrientedFunction) {
       finalChassisSpeeds =
-          ChassisSpeeds.fromFieldRelativeSpeeds(m_speeds.get(), m_SwerveSubsystem.getRotation2d());
+          ChassisSpeeds.fromFieldRelativeSpeeds(speeds, m_SwerveSubsystem.getRotation2d());
     } else {
-      finalChassisSpeeds = m_speeds.get();
+      finalChassisSpeeds = speeds;
     }
 
     // Set the swerve module states
-    SwerveModuleState[] moduleStates = kSwerve.kinematics.toSwerveModuleStates(finalChassisSpeeds);
-
-    // output to swerve modules
-    m_SwerveSubsystem.setModuleStates(moduleStates);
+    moduleStates = kSwerve.kinematics.toSwerveModuleStates(finalChassisSpeeds);
+    logger.logSwerveModuleState("states", moduleStates);
   }
 
   @Override
-  public void end(boolean interrupted) {}
-
-  @Override
-  public boolean isFinished() {
-    return false;
+  public void periodic() {
+    m_SwerveSubsystem.setModuleStates(moduleStates);
   }
 }
