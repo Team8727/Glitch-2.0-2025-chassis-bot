5739c4a2250c0feef5cd4fe5880f2785664914ec
ziggytron <ziggytron565@gmail.com>
2025-03-09 17:27:40 -0400

    controller 2

--- src/main/java/frc/robot/controller/Controller.java
+++ src/main/java/frc/robot/controller/Controller.java
@@ -6,19 +6,23 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  * Wrapper for a controller that can have a set of bindings applied to it.
  */
 public class Controller {
-    private final int m_port = 0;
-    CommandXboxController m_controller = new CommandXboxController(m_port);
+    private final int m_mainPort = 0;
+    private final int m_assistPort = 1;
+    CommandXboxController m_mainController = new CommandXboxController(m_mainPort);
+    CommandXboxController m_assistController = new CommandXboxController(m_assistPort);
     ControllerBindings m_currentBindings;
 
     public void applyBindings(ControllerBindings bindings) {
         if (m_currentBindings != null) {
-            m_currentBindings.unbind(m_controller);
+            m_currentBindings.unbind(m_mainController);
+            m_currentBindings.unbind(m_assistController);
         }
 
-        m_controller = new CommandXboxController(m_port);
+        m_mainController = new CommandXboxController(m_mainPort);
 
         if (bindings != null) {
-            bindings.bind(m_controller);
+            bindings.bind(m_mainController);
+            bindings.bind(m_assistController);
         }
 
         m_currentBindings = bindings;
